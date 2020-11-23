650ae4841dd3d86481eb1bb4272039a921657f46
VeronikaSkripko <70842201+VeronikaSkripko@users.noreply.github.com>
2020-11-23 19:08:27 +0300

    Delete MyArraySizeException.java

--- lesson2/src/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-import java.io.PrintWriter;
-
-public class MyArraySizeException extends Exception{
-	@Override
-	public void printStackTrace() {
-		System.out.println("Задан неверный размер массива");
-	}
-}
